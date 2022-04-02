dea1310f1da9d17e4e3bd479c2d7733eb2382b1e
geosearchef <geosearchef@googlemail.com>
2022-04-02 21:36:05 +0200

    only pull the required canvases

--- src/placer.py
+++ src/placer.py
@@ -9,6 +9,7 @@ from websocket import create_connection
 
 from boards.board_base import BoardBase
 from color import Color
+from target_configuration import target_configuration
 
 # based on https://github.com/goatgoose/PlaceBot and https://github.com/rdeepak2002/reddit-place-script-2022/blob/073c13f6b303f89b4f961cdbcbd008d0b4437b39/main.py#L316
 
