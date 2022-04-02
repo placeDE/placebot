fde8c566c3e51df38e5407ce182cbe718b87fc13
geosearchef <geosearchef@googlemail.com>
2022-04-02 21:41:39 +0200

    fallback if required canvases not specified

--- src/placer.py
+++ src/placer.py
@@ -9,7 +9,6 @@ from websocket import create_connection
 
 from boards.board_base import BoardBase
 from color import Color
-from target_configuration import target_configuration
 
 # based on https://github.com/goatgoose/PlaceBot and https://github.com/rdeepak2002/reddit-place-script-2022/blob/073c13f6b303f89b4f961cdbcbd008d0b4437b39/main.py#L316
 
@@ -176,6 +175,7 @@ class Placer:
     Uses the returned URL to request the actual image using HTTP
     :param canvas_id: the canvas to fetch
     """
+
     def update_canvas(self, canvas_id):
         print("Getting board")
         ws = create_connection("wss://gql-realtime-2.reddit.com/query")
