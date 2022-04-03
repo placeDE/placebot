cb28b81421d0f9b641a78067dd7b1b22a72ed18f
Per <atlasfreak@gmx.de>
2022-04-03 21:46:46 +0200

    added all new colors

--- src/color.py
+++ src/color.py
@@ -4,32 +4,38 @@ from PIL import ImageColor
 
 
 class Color(Enum):
-    DARK_RED = {"id": 1, "hex": "#BE0039"}
-    RED = {"id": 2, "hex": "#FF4500"}
-    ORANGE = {"id": 3, "hex": "#FFA800"}
-    YELLOW = {"id": 4, "hex": "#FFD635"}
-    PALE_YELLOW = {"id": 5, "hex": "#FFF8B8"}
-    DARK_GREEN = {"id": 6, "hex": "#00A368"}
-    GREEN = {"id": 7, "hex": "#00CC78"}
-    LIGHT_GREEN = {"id": 8, "hex": "#7EED56"}
-    DARK_TEAL = {"id": 9, "hex": "#00756F"}
-    LIGHT_TEAL = {"id": 10, "hex": "#009EAA"}
-    DARK_BLUE = {"id": 12, "hex": "#2450A4"}
-    BLUE = {"id": 13, "hex": "#3690EA"}
-    CYAN = {"id": 14, "hex": "#51E9F4"}
-    INDIGO = {"id": 15, "hex": "#493AC1"}
-    PERIWINKLE = {"id": 16, "hex": "#6A5CFF"}
-    DARK_PURPLE = {"id": 18, "hex": "#811E9F"}
-    PURPLE = {"id": 19, "hex": "#B44AC0"}
-    DARK_PINK = {"id": 22, "hex": "#FF3881"}
-    LIGHT_PINK = {"id": 23, "hex": "#FF99AA"}
-    DARK_BROWN = {"id": 24, "hex": "#6D482F"}
-    BROWN = {"id": 25, "hex": "#9C6926"}
-    BLACK = {"id": 27, "hex": "#000000"}
-    DARK_GREY = {"id": 28, "hex": "#515252"}
-    GREY = {"id": 29, "hex": "#898D90"}
-    LIGHT_GREY = {"id": 30, "hex": "#D4D7D9"}
-    WHITE = {"id": 31, "hex": "#FFFFFF"}
+    BURGUNDY = {"id": 1, "hex": "#6d001a"}
+    DARK_RED = {"id": 2, "hex": "#be0039"}
+    RED = {"id": 3, "hex": "#ff4500"}
+    ORANGE = {"id": 4, "hex": "#ffa800"}
+    YELLOW = {"id": 5, "hex": "#ffd635"}
+    PALE_YELLOW = {"id": 6, "hex": "#fff8b8"}
+    DARK_GREEN = {"id": 7, "hex": "#00a368"}
+    GREEN = {"id": 8, "hex": "#00cc78"}
+    LIGHT_GREEN = {"id": 9, "hex": "#7eed56"}
+    DARK_TEAL = {"id": 10, "hex": "#00756f"}
+    TEAL = {"id": 11, "hex": "#009eaa"}
+    LIGHT_TEAL = {"id": 12, "hex": "#00ccc0"}
+    DARK_BLUE = {"id": 13, "hex": "#2450a4"}
+    BLUE = {"id": 14, "hex": "#3690ea"}
+    LIGHT_BLUE = {"id": 15, "hex": "#51e9f4"}
+    INDIGO = {"id": 16, "hex": "#493ac1"}
+    PERIWINKLE = {"id": 17, "hex": "#6a5cff"}
+    LAVENDER = {"id": 18, "hex": "#94b3ff"}
+    DARK_PURPLE = {"id": 19, "hex": "#811e9f"}
+    PURPLE = {"id": 20, "hex": "#b44ac0"}
+    PALE_PURPLE = {"id": 21, "hex": "#e4abff"}
+    MAGENTA = {"id": 22, "hex": "#de107f"}
+    PINK = {"id": 23, "hex": "#ff3881"}
+    LIGHT_PINK = {"id": 24, "hex": "#ff99aa"}
+    DARK_BROWN = {"id": 25, "hex": "#6D482F"}
+    BROWN = {"id": 26, "hex": "#9C6926"}
+    BEIGE = {"id": 27, "hex": "#FFB470"}
+    BLACK = {"id": 28, "hex": "#000000"}
+    DARK_GRAY = {"id": 29, "hex": "#515252"}
+    GRAY = {"id": 30, "hex": "#898D90"}
+    LIGHT_GRAY = {"id": 31, "hex": "#D4D7D9"}
+    WHITE = {"id": 32, "hex": "#ffffff"}
 
 
 # generate rgb values for all colors
